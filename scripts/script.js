7f38453c5a8e31d591d65a95ce267762b2546132
Anastasia Tsiklauri <anastasia_tsiklauri@yahoo.com>
2026-04-26 22:03:44 +0400

    added a scrollbar with data from json and a reusable navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,47 @@
+// navbar data
+fetch('./components/navbar.html')
+.then(response => response.text())
+.then(data => {
+  document.querySelector('#header-container').innerHTML = data;
+});
+
+// get scrollbar data
+const row = document.getElementById('category-row');
+
+fetch('./datas/scrollbar.json')
+.then(response => response.json())
+.then(data => {
+
+    data.forEach(category => {
+
+        const card = document.createElement('div');
+
+        card.classList.add(
+            'category-item',
+            'text-center'
+        );
+
+        card.innerHTML = `
+            <div class="category-image-placeholder">
+                <img src="${category.image}" alt="${category.name}">
+            </div>
+
+            <h6 class="mt-3 mb-1 category-name">
+                ${category.name}
+            </h6>
+
+            <small class="text-muted item-count">
+                ${category.items}+ items
+            </small>
+        `;
+
+        row.appendChild(card);
+
+    });
+
+});
+
+// loading animation
 window.addEventListener("load", function () {
     const loader = document.getElementById("loader");
 
@@ -6,6 +50,7 @@ window.addEventListener("load", function () {
     }, 1500);
 });
 
+// pop up modal window
 function delay(ms) {
     return new Promise((resolve) => {
         setTimeout(() => {
